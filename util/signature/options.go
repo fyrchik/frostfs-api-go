nspcc-dev/neofs-api-go

[#270] v2/signature: allow to provide custom unmarshaling function
API user can implement his/her own cache.
Functional options pattern is hard to reuse between different packages,
so put `SignOption` in `util/signature` and make `Options` public.

Signed-off-by: Evgenii Stratonikov <evgeniy@nspcc.ru>

util/signature/options.go
@@ -8,22 +8,32 @@ import (
 	crypto "github.com/nspcc-dev/neofs-crypto"
 )
 
-type cfg struct {
-	signFunc   func(key *ecdsa.PrivateKey, stream DataSource) ([]byte, error)
-	verifyFunc func(key *ecdsa.PublicKey, stream DataSource, sig []byte) error
+type Options struct {
+	SignFunc        func(key *ecdsa.PrivateKey, stream DataSource) ([]byte, error)
+	VerifyFunc      func(key *ecdsa.PublicKey, stream DataSource, sig []byte) error
+	UnmarshalPublic func([]byte) *ecdsa.PublicKey
 }
 
-func defaultCfg() *cfg {
-	return &cfg{
-		signFunc:   cryptoSign,
-		verifyFunc: cryptoVerify,
+// DefaultOptions represents default set of options.
+func DefaultOptions() *Options {
+	return &Options{
+		SignFunc:        cryptoSign,
+		VerifyFunc:      cryptoVerify,
+		UnmarshalPublic: crypto.UnmarshalPublicKey,
 	}
 }
 
 func SignWithRFC6979() SignOption {
-	return func(c *cfg) {
-		c.signFunc = rfc6979Sign
-		c.verifyFunc = rfc6979Verify
+	return func(c *Options) {
+		c.SignFunc = rfc6979Sign
+		c.VerifyFunc = rfc6979Verify
+	}
+}
+
+// WithUnmarshalPublicKey sets f as a function for unmarshaling public keys.
+func WithUnmarshalPublicKey(f func([]byte) *ecdsa.PublicKey) SignOption {
+	return func(c *Options) {
+		c.UnmarshalPublic = f
 	}
 }
 

fyrchik/frostfs-api-go

util/signature/options.go
@@ -9,12 +9,16 @@ import (
 type cfg struct {
 	signFunc   func(key *ecdsa.PrivateKey, msg []byte) ([]byte, error)
 	verifyFunc func(key *ecdsa.PublicKey, msg []byte, sig []byte) error
+	// unmarshalPublic is function used to unmarshal public key.
+	unmarshalPublic func([]byte) *ecdsa.PublicKey
 }
 
+// defaultCfg represents default set of options.
 func defaultCfg() *cfg {
 	return &cfg{
-		signFunc:   crypto.Sign,
-		verifyFunc: crypto.Verify,
+		signFunc:        crypto.Sign,
+		verifyFunc:      crypto.Verify,
+		unmarshalPublic: crypto.UnmarshalPublicKey,
 	}
 }
 
@@ -22,5 +26,12 @@ func SignWithRFC6979() SignOption {
 	return func(c *cfg) {
 		c.signFunc = crypto.SignRFC6979
 		c.verifyFunc = crypto.VerifyRFC6979
+	}
+}
+
+// WithUnmarshalPublicKey sets f as a function for unmarshaling public keys.
+func WithUnmarshalPublicKey(f func([]byte) *ecdsa.PublicKey) SignOption {
+	return func(c *cfg) {
+		c.unmarshalPublic = f
 	}
 }